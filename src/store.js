1c49d5f0b3e5b0cc5d96687b1a4b151b09d5e5aa
Shamsul <48136141+shkms11@users.noreply.github.com>
2024-12-25 01:22:55 +0600

    Remove deprecated socketSlice.js and store.js files

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import socketReducer from "./socketSlice";
-
-export const store = configureStore({
-    reducer: {
-        socket: socketReducer,
-    },
-});
