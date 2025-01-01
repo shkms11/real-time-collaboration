605844cc1867311959262e02d6b561086880082a
Shamsul <48136141+shkms11@users.noreply.github.com>
2025-01-01 22:44:28 +0600

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import "./input.css";
 import { Provider } from "react-redux";
-import store from "./store";
-import RoomList from "./RoomList";
-import Chat from "./Chat";
+import store from "./redux/store";
+
+import Room from "./Room";
+// import RoomList from "./RoomList";
+// import Chat from "./Chat";
 
 function App() {
     return (
         <Provider store={store}>
             <div className="App">
-                <RoomList />
-                <Chat />
+                <Room />
             </div>
         </Provider>
     );
